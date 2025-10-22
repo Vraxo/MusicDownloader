5deb4f9bdf7cd5faae99be79232234aad5b0b306
Vraxo <pkafsharix@gmail.com>
2025-10-22 15:40:21 +0330

    Stable

--- MusicDownloader/Source/AppSettings.cs
+++ MusicDownloader/Source/AppSettings.cs
@@ -3,15 +3,15 @@
 public static class AppSettings
 {
     // Paths
-    public const string CsvDir = "D:\\Parsa Stuff\\Documents\\Music Collections";
-    public const string BaseDataDir = "D:\\Parsa Stuff\\Audio\\Music";
+    public const string CsvDir = "E:\\Parsa Stuff\\Documents\\Music Collections";
+    public const string BaseDataDir = "E:\\Parsa Stuff\\Audio\\Music";
     public const string CookieFile = "Data\\Cookies.txt";
     public const string PlaylistCheckReportFile = "Data\\PlaylistReport.txt";
 
     // Executables
     public const string YtDlpExe = "yt-dlp";
     public const string FfmpegExe = "ffmpeg";
-    public const string FallbackYtDlpExe = @"D:\Program Files\yt-dlp\yt-dlp.exe";
+    public const string FallbackYtDlpExe = @"C:\Program Files\yt-dlp\yt-dlp.exe";
 
     // Audio Settings
     public const string AudioFormat = "m4a";
